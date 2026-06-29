e5967fc62add1b91d9675831312d91388f9fe3fd
Sahan <sahandinusha152b3@gmail.com>
2026-06-29 17:08:31 +0530

    Re create the login layout and bulid the left side container

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,13 +4,21 @@ import "./Login.css";
 function Login() {
   return (
     <div className="auth-page">
-      <div classname ="auth-container">
-        <div classname ="auth-left">
-          Left section
+      <div className="auth-card">
+        <div className="auth-left-panel">
+          <button className="auth-back-btn">← Back</button>
+
+          <div className="auth-left-content">
+            <h1>Welcome Back!</h1>
+            <p>
+              Access your SmartQueue account to order delicious meals from your
+              favorite campus canteens.
+            </p>
+          </div>
         </div>
 
-        <div classname ="auth-right">
-          Right selection
+        <div className="auth-right-panel">
+          Right side content
         </div>
       </div>
     </div>
